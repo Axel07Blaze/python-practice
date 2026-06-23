04737d32d4f262960a91065d54cde1f09f82fb4c
Axel07Blaze <146859071+Axel07Blaze@users.noreply.github.com>
2026-06-23 17:25:12 +0530

    Delete simple_intrest.py

--- simple_intrest.py
+++
@@ -1,8 +0,0 @@
-# Simple intrest calculator
-P = int(input("Enter principal amoutn:"))
-R = int(input("Enter Rate of intrest:"))
-T = int(input("Enter Time period:"))
-
-#SI 
-SI = ((P*R*T)/100)
-print ("Simple intrest is :",SI)
