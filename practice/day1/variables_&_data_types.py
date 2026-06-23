9e611a8209a5bf99c82cec8ecfd19a0e4df3c1dc
Axel07Blaze <146859071+Axel07Blaze@users.noreply.github.com>
2026-06-23 17:34:14 +0530

    Create variables_&_data_types.py

---
+++ practice/day1/variables_&_data_types.py
@@ -0,0 +1,15 @@
+# variables
+name = "axel"
+age = 22
+print (name) 
+print (age)
+
+#data types
+age = 22
+name ="Axel"
+height = 5.6
+is_student = True
+print (int(age))
+print (str(name))
+print (float(height))
+print (bool(is_student))
