10ad4af811e416ff59b6f31dd1fff75e90120165
Axel07Blaze <146859071+Axel07Blaze@users.noreply.github.com>
2026-06-23 17:33:41 +0530

    Create operators.py

---
+++ practice/day1/operators.py
@@ -0,0 +1,26 @@
+#addition
+a = 10
+b = 5
+print (a + b)
+
+#subtraction
+a = 8
+b = 19
+print(a - b)
+
+#multiplication
+
+a = 13
+b = 19
+print(a*b)
+
+#division
+a = 14
+b = 2
+print (a/b)
+
+#modulus
+a = 10
+b = 2
+
+print (a%b)
