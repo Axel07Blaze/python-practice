2d6c3659b532cf96e8e798e3a6757e88b6aa29cc
Axel07Blaze <146859071+Axel07Blaze@users.noreply.github.com>
2026-06-23 17:12:24 +0530

    Add files via upload
    day 2 practice included
    if
    else
    elif
    while loop

---
+++ voting_eligibility_using_if_else.py
@@ -0,0 +1,15 @@
+'''  "if and else statement" it is condition based statement where we say if this condition 
+is true then do this or run the else case '''
+age = int(input("what is your age:"))
+
+if age >= 18:
+    print ("you can vote")
+else:
+    print("you cannot vote")    
+  
+
+
+
+
+
+
---
+++ while_loop.py
@@ -0,0 +1,7 @@
+# learning while loop 
+# write a program to print 1,2,3,4,5,6,7,8,9,10
+count = 1
+while count <=10:
+    print(count)
+    count = (count+1)
+       
