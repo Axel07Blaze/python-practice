9f5fae2cb54363b9dcfa54b725765b137ad84b24
Axel07Blaze <146859071+Axel07Blaze@users.noreply.github.com>
2026-06-23 17:37:56 +0530

    Create forloop.py

---
+++ practice/day2/forloop.py
@@ -0,0 +1,14 @@
+# for how loop works
+for i in range(5):
+    print ("axel")
+
+for i in range(5):
+    print(i)
+
+for i in range(3):
+    print("Hello")
+
+
+numbers = [10,20,30,40,50]
+for num in numbers:
+    print ("the numbers are:",num)
