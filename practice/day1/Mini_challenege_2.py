2c3105964abee2c5b2c55423ab57322a03e7f3e7
Axel07Blaze <146859071+Axel07Blaze@users.noreply.github.com>
2026-06-23 17:33:12 +0530

    Create Mini_challenege_2.py

---
+++ practice/day1/Mini_challenege_2.py
@@ -0,0 +1,25 @@
+'''Now do Mini Challenge 2:
+num1 = 20
+num2 = 10
+
+
+Print:
+
+Addition
+Subtraction
+Multiplication
+Division'''
+num1 = 20
+num2 = 10
+#addition
+print(num1 + num2)
+
+#subtraction
+print(num1 - num2)
+
+#multiplication
+print(num1*num2)
+
+#division
+print(num1/num2)
+
