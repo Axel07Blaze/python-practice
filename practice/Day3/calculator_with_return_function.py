d892750c2da021b502646e1625c2c9228aa5e745
Axel <146859071+Axel07Blaze@users.noreply.github.com>
2026-06-26 16:56:26 +0530

    Create calculator_with_return_function.py

---
+++ practice/Day3/calculator_with_return_function.py
@@ -0,0 +1,18 @@
+def calci (a, b):
+    add = (a+b)
+    div = (a/b)
+    mul = (a*b)
+    sub = (a-b)
+    return add,div,mul,sub
+    
+
+addition,division,multiplication,subtraction = calci(20,30)
+
+print("addition:",addition)
+print("division:",division)
+print("multiplication:",multiplication)
+print("subtraction:",subtraction)
+
+
+
+          
