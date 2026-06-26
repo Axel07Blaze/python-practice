429dd5e62ef28e254442e6c449e3ec2f991be3a9
Axel <146859071+Axel07Blaze@users.noreply.github.com>
2026-06-26 16:58:17 +0530

    Create mini_challenge_3.py

---
+++ practice/Day3/mini_challenge_3.py
@@ -0,0 +1,9 @@
+#write a function with 2 parameters
+
+def plan(name,game):
+    print("Hii welcome to ",name)
+    print("Today we're playing ",game)
+
+plan("AxelTv","valornt")
+plan("Tenz","Minecraft")    
+plan("Sarasafari","gym simulator")
