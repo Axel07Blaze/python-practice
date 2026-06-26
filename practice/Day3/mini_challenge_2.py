c4f60f59b2829701474d13bc9f864ddf79ac992e
Axel <146859071+Axel07Blaze@users.noreply.github.com>
2026-06-26 16:58:02 +0530

    Create mini_challenge_2.py

---
+++ practice/Day3/mini_challenge_2.py
@@ -0,0 +1,9 @@
+#write an modifiable function where u can change channel name by taking input 
+
+def streamer(channel_name):
+    print("Hello this is",channel_name)
+
+
+streamer("AxelTV")
+streamer("ninja")
+streamer("Tenz")
