bdd880fccd0092594e8e2763283be5ca8600dd76
Axel <146859071+Axel07Blaze@users.noreply.github.com>
2026-06-26 16:57:49 +0530

    Create mini _challenge1.py

---
+++ practice/Day3/mini _challenge1.py
@@ -0,0 +1,8 @@
+#write an intro for ur channel and print it twice usinf for loop
+def stream_intro():
+    print("Hello, Welcome to my Channel")
+    print("AxelTV, we play Valorant a lot here")
+    print ("Hope u will enjoy your stay")
+
+for i in range(2):
+    stream_intro()  
